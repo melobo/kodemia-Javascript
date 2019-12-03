8151d74ed6c92f03d692f3aa860e9af704357dd7
melobo <warchocobo@gmail.com>
2019-12-02 22:09:17 -0600

    ejercicio en clase

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log('Hello Koders!')
+var pesosMexicanos = 20
+var tipoCambioUSD = 19.57
+var tipoCambioEUR = 21.67
+var tipoCambioCUC = 26.5 / 0.0511285
+
+console.log('Tengo ' + pesosMexicanos / tipoCambioUSD + ' USD')
+console.log('Tengo ' + pesosMexicanos / tipoCambioEUR + ' EUR')
+console.log('Tengo ' + pesosMexicanos * tipoCambioCUC + ' CUC')
