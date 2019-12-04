334e8b9b5b3a4cede27cc824d744f941c1a451d6
melobo <warchocobo@gmail.com>
2019-12-03 18:36:23 -0600

    Add age homework

--- js/main.js
+++ js/main.js
@@ -6,3 +6,12 @@ var tipoCambioCUC = 26.5 / 0.0511285
 console.log('Tengo ' + pesosMexicanos / tipoCambioUSD + ' USD')
 console.log('Tengo ' + pesosMexicanos / tipoCambioEUR + ' EUR')
 console.log('Tengo ' + pesosMexicanos * tipoCambioCUC + ' CUC')
+
+/* Ejercicio Tarea, edad en segundos*/
+
+var ageYears = 30
+var ageMonths = 8
+var ageDays = 32
+var ageSeconds = (((ageYears * 365 + ageMonths * 30 + ageDays) * 24) * 60) * 60
+
+console.log('Tengo ' + ageYears + ' con ' + ageMonths + 'meses y ' + ageDays + ' días. Por lo tanto tengo ' + ageSeconds + ' segundos y contando.')
