276f9ee71e8f94b856b5d250fe9c070fa6c7a794
melobo <warchocobo@gmail.com>
2019-12-03 20:36:02 -0600

    Ejercicio dos

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
-/*Ejercicio Uno*/
+/** Ejercicio uno */
 
 var word = "Perros"
 var wordSingular = word[0] + word[1] + word[2] + word[3] + word[4]
 
 console.log(wordSingular)
+
+/** Ejercicio dos */
+
+var number = 30
+
+if (number >= 0 && number <= 100 && number % 10 === 0) {
+    console.log('El número esta entre 0 y 100 y ademas es multiplo de 10')
+}
