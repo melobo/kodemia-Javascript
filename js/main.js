555455710ffb7499fe3843233b32477375430449
melobo <warchocobo@gmail.com>
2019-12-04 20:24:05 -0600

    Ejercicio uno

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log('Hello Koders!')
+/** Ejercicio uno */
+
+var numbers = [1, 2, 3, 4, 5]
+var position = 0
+
+while (position < numbers.length) {
+    console.log(numbers[position])
+    position++
+}
+
+position = numbers.length
+
+while (position >= 0) {
+    console.log(numbers[position])
+    position--
+}
