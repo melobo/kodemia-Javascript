76cb9541aa8e4e863faf45797dc3871c1005dfe7
melobo <warchocobo@gmail.com>
2019-12-08 18:41:34 -0600

    Ejercicios en clase

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-console.log('Hello Koders!')
+// Ejercicio uno
+
+function capitalizer(phrase) {
+    var stringToArray = phrase.toLowerCase().split(' ')
+    var arrayCapitalize = []
+    for (var index = 0; index < stringToArray.length; index++) {
+        var word = stringToArray[index]
+        var wordCapitalize = `${word[0].toUpperCase()}${word.slice(1)}`
+        arrayCapitalize[index] = wordCapitalize
+    }
+    var arrayToString = arrayCapitalize.join(' ')
+    return arrayToString
+}
