10b7b186fd01457ab7f2d4873074bac7db4da9f3
melobo <warchocobo@gmail.com>
2019-12-09 20:43:28 -0600

    Ejercicio .map .forEach

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log('Hello Koders!')
+// Metodo .map
+
+var numbers = [[1, 2, 4], [4, 3, 5], [5, 6, 5, 6]]
+
+numbers.map(function (arr) {
+    let sum = 0
+    arr.forEach(function (number) {
+        sum += number
+    })
+    return sum
+})
