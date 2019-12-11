eff5c5ddea93eb122a553fff1eb2b0ca940b0ef5
melobo <warchocobo@gmail.com>
2019-12-10 18:56:31 -0600

    Ejercicios de la clase

--- js/main.js
+++ js/main.js
@@ -9,3 +9,42 @@ numbers.map(function (arr) {
     })
     return sum
 })
+
+// Ejercicio uno
+
+var words = ['Asshole', 'armand', 'love', 'faith']
+
+words.filter(function (word) {
+    return word[0] === 'a' || word[0] === 'A'
+})
+
+// Ejercicio dos
+
+var numbers = [2, 4]
+
+var filtered = numbers.filter(function (number) {
+    return number % 2 === 0 && number >= 0
+})
+
+if (numbers.length === filtered.length) {
+    console.log(`Todos elementos de ${numbers} son pares y positivos`)
+} else {
+    console.log(`No todos elementos de ${numbers} son pares y positivos`)
+}
+
+// Ejercicio tres
+
+numbers.map(function (number) {
+    return number ** 2
+})
+
+// Ejercicio cuatro
+
+var numbers = [[1, 2, 3, 0], [10, 0, 3, 4], [1, 2, 0], [4, 3, 2, 1]]
+const reduceMatrix = matrix => {
+    return matrix.reduce((acc, cValue) => {
+        return acc + cValue.reduce((acc2, cValue2) => acc2 + cValue2, 0)
+    }, 0)
+}
+console.log(reduceMatrix(numbers))
+// Output -> 36
