c8449eabd768851326c38fed0b2700547eb9140f
melobo <warchocobo@gmail.com>
2019-12-19 18:57:47 -0600

    DOM selectors JSON

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log('Hello Koders!')
+// var menu = ['perro1', 'perro2', 'perro3']
+// var children = menu.reduce((acc, cValue) => acc + `<li>${cValue}</li>`, '')
+// var parent = document.getElementsByTagName('ul')[0]
+// parent.innerHTML = children
+
+// var liNodes = document.getElementsByTagName('li')
+
+// const deactivateMenuItems = () => {
+//     for (let index = 0; index < liNodes.length; index++) {
+//         liNodes[index].classList.remove('active')
+//     }
+// }
+
+// for (let index = 0; index < liNodes.length; index++) {
+//     liNodes[index].addEventListener('click', function () {
+//         deactivateMenuItems()
+//         this.classList.add('active')
+//     })
+// }
