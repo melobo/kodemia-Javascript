1ec0350e1e5855c8f0b3781ae4df98a9169c4452
melobo <warchocobo@gmail.com>
2019-12-20 19:35:37 -0600

    14

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-console.log('Hello Koders!')
+var posts = {
+    'firstName': 'Cesar',
+    'lastName': 'Villegas',
+    'age': 30,
+    'pitch': false
+}
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+        responseHandler(this.response)
+    }
+}
+
+// xhttp.open('POST', 'https://ajaxkode.firebaseio.com/koders.json')
+// xhttp.send(JSON.stringify(posts))
+
+xhttp.open('GET', 'https://ajaxkode.firebaseio.com/koders.json')
+xhttp.send()
+
+const responseHandler = response => {
+}
+
+var parentNode = document.getElementsByClassName('container')[0]
+
